5553009656a2d91f8dadee38d3f324b167f15f71
Clarionxk <Clarion.xk@aol.com>
2024-01-08 19:51:17 +0200

    first commit

---
+++ TikTok.py
@@ -0,0 +1,94 @@
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+import requests
+from urllib.request import urlopen
+
+
+
+def downloadVideo(link, id):
+    cookies = {
+        '__cflb': '0H28v8EEysMCvTTqtuFFMWyYEmbm6aBg3YFPx5Y3puj',
+    }
+
+    headers = {
+        'authority': 'ssstik.io',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        # 'cookie': '__cflb=0H28v8EEysMCvTTqtuFFMWyYEmbm6aBg3YFPx5Y3puj',
+        'hx-current-url': 'https://ssstik.io/en',
+        'hx-request': 'true',
+        'hx-target': 'target',
+        'hx-trigger': '_gcaptcha_pt',
+        'origin': 'https://ssstik.io',
+        'referer': 'https://ssstik.io/en',
+        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    }
+
+    params = {
+        'url': 'dl',
+    }
+
+    data = {
+        'id': link,
+        'locale': 'en',
+        'tt': 'TVNMM0tj',
+    }
+
+    response = requests.post('https://ssstik.io/abc', params=params, cookies=cookies, headers=headers, data=data)
+    downloadSoup = BeautifulSoup(response.text, "html.parser")
+
+    downloadLink = downloadSoup.a["href"]
+
+    mp4File = urlopen(downloadLink)
+    with open(f"videos/{id}.mp4", "wb") as output:
+        while True:
+            data = mp4File.read(4096)
+            if data:
+                output.write(data)
+            else:
+                break
+
+
+
+
+
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.tiktok.com/@tina_mt__")
+
+time.sleep(15)
+
+#####
+scroll_pause_time = 1
+screen_height = driver.execute_script("return window.screen.height;")
+i = 1
+
+print("STEP 2: Scrolling page")
+while True:
+    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+    i += 1
+    time.sleep(scroll_pause_time)
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+    if (screen_height) * i > scroll_height:
+        break 
+
+#####
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+videos = soup.find_all("div", {"class": "tiktok-16ou6xi-DivTagCardDesc"})
+
+print(len(videos))
+for index, video in enumerate(videos):
+    downloadVideo(video.a["href"], index)
+    time.sleep(10)
+
